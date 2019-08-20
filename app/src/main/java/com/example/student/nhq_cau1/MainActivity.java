bd28e7f6ee66a81cef32d4afb3e40ecedd21c58c
Dong <fantasywhite97@gmail.com>
2019-08-20 17:10:31 +0700

    Message

---
+++ app/src/main/java/com/example/student/nhq_cau1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.student.nhq_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB;
+    Button btnTong;
+    TextView textViewTong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtA = (EditText) findViewById(R.id.edtA);
+        edtB = (EditText) findViewById(R.id.edtB);
+        textViewTong = (TextView) findViewById(R.id.textViewTong);
+        btnTong = (Button) findViewById(R.id.btnTong);
+
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String chuoi1, chuoi2;
+                int num_a = Integer.parseInt(edtA.getText().toString());
+                int num_b = Integer.parseInt(edtB.getText().toString());
+                int tong = num_a+ num_b;
+
+                textViewTong.setText(String.valueOf(tong));
+
+            }
+        });
+    }
+}
